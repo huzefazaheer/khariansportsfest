4618faea851a004335e58898a1983156ae01e225
Huzefa <huzefazaheer1+git@gmail.com>
2024-03-01 20:17:00 +0500

    Added registeration page

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.send("Register!");
+  res.sendFile(path.join(__dirname, "public/registerpage/index.html"));
 });
 
 app.listen(port, () => {

54b7935d77cf5bd9c7930092c271dbf9928613cd
Huzefa <huzefazaheer1+git@gmail.com>
2024-03-01 14:12:53 +0500

    Setup express server

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public/homepage"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/homepage/index.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.send("Register!");
+});
+
+app.listen(port, () => {
+  console.log("Server started on http://localhost:3000");
+});

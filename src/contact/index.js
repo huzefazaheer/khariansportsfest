ad169dc0e0b898c5c149091c96ea4f6c49d0cae3
Huzefa <huzefazaheer1+git@gmail.com>
2024-03-26 04:16:05 +0500

    Added realtime database

--- src/contact/index.js
+++ src/contact/index.js
@@ -1,1 +1,28 @@
 import "./style.css";
+
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL:
+    "https://khariansportsfest-fde2f-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
+function writeUserData(userId, name, email, complaint) {
+  const db = getDatabase();
+  set(ref(db, "users/" + userId), {
+    username: name,
+    email: email,
+    complaint: complaint,
+  });
+}

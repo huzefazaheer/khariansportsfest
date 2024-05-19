fd748e25c6e294f9d942ccdaa5999fa66d3eaf15
Huzefa <huzefazaheer1+git@gmail.com>
2024-05-19 17:48:56 +0500

    Added priceing

--- src/react/components/form/ordersummary.js
+++ src/react/components/form/ordersummary.js
@@ -8,6 +8,18 @@ export default function OrderSummary({ personData, setPersonData }) {
 		sportslist = sportslist + personData.sports[i].label + '\n'
 	}
 
+	function calculateTotal() {
+		let total = 2000
+		if (personData.socials.label == 'Yes' && personData.sports.length >= 1) {
+			total = total + 1000
+		}
+		if (personData.sports.length > 1) {
+			total = total + 500 * (personData.sports.length - 1)
+		}
+
+		return total
+	}
+
 	return (
 		<>
 			<div className="formcontainer">
@@ -19,6 +31,8 @@ export default function OrderSummary({ personData, setPersonData }) {
 				<p>CNIC: {personData.cnic}</p>
 				<p>Socials included: {personData.socials.label}</p>
 				<p>Games: {sportslist}</p>
+
+				<h5>Your total is: {calculateTotal()}</h5>
 			</div>
 		</>
 	)

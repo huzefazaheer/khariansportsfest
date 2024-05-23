73a7652c804341c67137697db145dd03bbabe399
Huzefa <huzefazaheer1+git@gmail.com>
2024-05-24 02:56:27 +0500

    Added pictures to team page and fixed styling conflicts

--- src/react/pages/about/about.js
+++ src/react/pages/about/about.js
@@ -3,32 +3,33 @@ import './about.css'
 export default function About() {
 	return (
 		<>
-			<div className="wrapper">
-				<div className="textss">
-					<article>
-						<h1>About Us</h1>
-						<img src="resources/logo.webp" alt="" />
-						<div className="texttwo">
-							{/* could write Our Dedicated Directors were once sitting on a beach and the idea to host a sports event clicked one of em mind. thats how KSF was born. */}
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-							maxime id eveniet, harum quo dicta perspiciatis voluptate corporis
-							neque cum minus temporibus, excepturi debitis adipisci dignissimos
-							incidunt porro nobis asperiores.
-						</div>
-						<h5>KSF</h5>
-						<p>
-							{/* Could write. We are hosting this even on 26th of june we have alot of stuff to do if u want to learn more about what we are offering you click ahref here */}
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui a
-							deleniti perspiciatis doloribus quaerat placeat! Dignissimos nam a
-							eaque ex deleniti minima ea maxime dolorum officiis aliquam? Modi
-							magni magnam architecto veniam labore veritatis accusantium! Ipsum
-							accusamus cupiditate, culpa ipsam porro perferendis impedit
-							doloremque dicta!
-						</p>
-					</article>
+			<div className="aboutsec">
+				<div className="wrapper">
+					<div className="textss">
+						<article>
+							<h1>About Us</h1>
+							<img src="resources/logo.webp" alt="" />
+							<div className="texttwo">
+								{/* could write Our Dedicated Directors were once sitting on a beach and the idea to host a sports event clicked one of em mind. thats how KSF was born. */}
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+								Sapiente maxime id eveniet, harum quo dicta perspiciatis
+								voluptate corporis neque cum minus temporibus, excepturi debitis
+								adipisci dignissimos incidunt porro nobis asperiores.
+							</div>
+							<h5>KSF</h5>
+							<p>
+								{/* Could write. We are hosting this even on 26th of june we have alot of stuff to do if u want to learn more about what we are offering you click ahref here */}
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui a
+								deleniti perspiciatis doloribus quaerat placeat! Dignissimos nam
+								a eaque ex deleniti minima ea maxime dolorum officiis aliquam?
+								Modi magni magnam architecto veniam labore veritatis
+								accusantium! Ipsum accusamus cupiditate, culpa ipsam porro
+								perferendis impedit doloremque dicta!
+							</p>
+						</article>
+					</div>
 				</div>
-			</div>
-			{/* <section>
+				{/* <section>
 				<div className="second">
 					<h1>What We Offer</h1>
 					<h2>We offering a variety of games.</h2>
@@ -55,6 +56,7 @@ export default function About() {
 					</p>
 				</div>
 			</section> */}
+			</div>
 		</>
 	)
 }
--- src/react/pages/team/team.js
+++ src/react/pages/team/team.js
@@ -4,147 +4,93 @@ export default function Team() {
 	return (
 		<>
 			<div className="teamsec">
-				<div className="imgg">
-					<img src="/img/logo.webp" alt="" />
+				<div className="teamcontainer">
+					<div className="row">
+						<h1>Our Team</h1>
+						<p className="futura-lt">
+							Meet the formidable team behind the Kharian Sports Fest, a dynamic
+							group of organizers dedicated to delivering an unparalleled
+							athletic extravaganza. With a passion for sports and a commitment
+							to excellence, they work tirelessly to ensure every aspect of the
+							event is executed flawlessly, promising an unforgettable
+							experience for all participants and spectators.
+						</p>
+					</div>
+					<div className="row imgrow">
+						<Personimg
+							imgsrc={
+								'https://firebasestorage.googleapis.com/v0/b/khariansportsfest-fde2f.appspot.com/o/ahsan.jpg?alt=media&token=20435fdf-b580-4382-91ba-0b607197bc8f'
+							}
+							name={'Ahsan Raza'}
+							title={'Director KSF'}
+						/>
+						<Personimg
+							imgsrc={
+								'https://firebasestorage.googleapis.com/v0/b/khariansportsfest-fde2f.appspot.com/o/hamza.jpg?alt=media&token=d9557e83-c557-4edd-a197-577871f85e64'
+							}
+							name={'Hamza Ahmed Noor'}
+							title={'Director KSF'}
+						/>
+						<Personimg
+							imgsrc={
+								'https://firebasestorage.googleapis.com/v0/b/khariansportsfest-fde2f.appspot.com/o/moiz.jpg?alt=media&token=1a8c134c-1c38-4631-81a3-aed1edae869b'
+							}
+							name={'Moiz Akhtar'}
+							title={'Director KSF'}
+						/>
+						<div style={{ clear: 'both' }} />
+					</div>
+					<div className="row imgrow">
+						<Personimg
+							imgsrc={
+								'https://firebasestorage.googleapis.com/v0/b/khariansportsfest-fde2f.appspot.com/o/imad.jpg?alt=media&token=75e0044e-e46a-4df6-a9dd-d9ec59fcacd5'
+							}
+							name={'Imad Brother'}
+							title={'Director Finance'}
+						/>
+						<Personimg
+							imgsrc={
+								'https://firebasestorage.googleapis.com/v0/b/khariansportsfest-fde2f.appspot.com/o/murtaza.jpg?alt=media&token=7b5aad92-9cd9-4562-9664-1c3c798afb8b'
+							}
+							name={'Murtaza Ahmed Abbas Naqvi'}
+							title={'Director Logistics'}
+						/>
+						<Personimg
+							imgsrc={
+								'https://firebasestorage.googleapis.com/v0/b/khariansportsfest-fde2f.appspot.com/o/wasti.jpg?alt=media&token=b685f07a-b5a8-4ce3-8c74-de9dd7520817'
+							}
+							name={'Wasti Lethargic'}
+							title={'Director Registerations'}
+						/>
+						<Personimg
+							imgsrc={
+								'https://firebasestorage.googleapis.com/v0/b/khariansportsfest-fde2f.appspot.com/o/huzefa.jpg?alt=media&token=8a8b07a4-59b3-4f8a-b13d-5a0faa1f2733'
+							}
+							name={'Huzefa Zaheer'}
+							title={'Director IT'}
+						/>
+
+						<div style={{ clear: 'both' }} />
+					</div>
 				</div>
-				<sec className="team">
-					<div className="teamcontainer">
-						<div className="row">
-							<h1>Our Team</h1>
-							<p className="futura-lt">
-								Meet the formidable team behind the Kharian Sports Fest, a
-								dynamic group of organizers dedicated to delivering an
-								unparalleled athletic extravaganza. With a passion for sports
-								and a commitment to excellence, they work tirelessly to ensure
-								every aspect of the event is executed flawlessly, promising an
-								unforgettable experience for all participants and spectators.
-							</p>
-						</div>
-						<div className="row">
-							<div className="coloumn">
-								<div className="imgbox">
-									<img
-										src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg"
-										alt=""
-									/>
-								</div>
-								<div className="details">
-									<h3>
-										Ahsan Raza <br />
-										<span>Director KSF</span>{' '}
-									</h3>
-								</div>
-							</div>
-							<div className="coloumn">
-								<div className="imgbox">
-									<img
-										src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg"
-										alt=""
-									/>
-								</div>
-								<div className="details">
-									<h3>
-										Hamza Ahmed Noor
-										<br />
-										<span>Director KSF</span>{' '}
-									</h3>
-								</div>
-							</div>
-							<div className="coloumn">
-								<div className="imgbox">
-									<img
-										src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg"
-										alt=""
-									/>
-								</div>
-								<div className="details">
-									<h3>
-										Moiz Akhter <br />
-										<span>Director KSF</span>{' '}
-									</h3>
-								</div>
-							</div>
-							<div className="coloumn">
-								<div className="imgbox">
-									<img
-										src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg"
-										alt=""
-									/>
-								</div>
-								<div className="details">
-									<h3>
-										Member Name <br />
-										<span>CO Director IT</span>{' '}
-									</h3>
-								</div>
-							</div>
-							<div style={{ clear: 'both' }} />
-						</div>
-					</div>
-				</sec>
-				<sec className="team">
-					<div className="teamcontainer">
-						<div className="row">
-							<div className="coloumn">
-								<div className="imgbox">
-									<img
-										src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg"
-										alt=""
-									/>
-								</div>
-								<div className="details">
-									<h3>
-										Member Name <br />
-										<span>CO Director IT</span>{' '}
-									</h3>
-								</div>
-							</div>
-							<div className="coloumn">
-								<div className="imgbox">
-									<img
-										src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg"
-										alt=""
-									/>
-								</div>
-								<div className="details">
-									<h3>
-										Member Name <br />
-										<span>CO Director IT</span>{' '}
-									</h3>
-								</div>
-							</div>
-							<div className="coloumn">
-								<div className="imgbox">
-									<img
-										src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg"
-										alt=""
-									/>
-								</div>
-								<div className="details">
-									<h3>
-										Member Name <br />
-										<span>CO Director IT</span>{' '}
-									</h3>
-								</div>
-							</div>
-							<div className="coloumn">
-								<div className="imgbox">
-									<img
-										src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg"
-										alt=""
-									/>
-								</div>
-								<div className="details">
-									<h3>
-										Member Name <br />
-										<span>CO Director IT</span>{' '}
-									</h3>
-								</div>
-							</div>
-							<div style={{ clear: 'both' }} />
-						</div>
-					</div>
-				</sec>
+			</div>
+		</>
+	)
+}
+
+function Personimg({ imgsrc, name, title }) {
+	return (
+		<>
+			<div className="coloumn">
+				<div className="imgbox">
+					<img src={imgsrc} alt="picture of person" />
+				</div>
+				<div className="details">
+					<h3>
+						<span className="name">{name}</span> <br />
+						<span className="title">{title}</span>{' '}
+					</h3>
+				</div>
 			</div>
 		</>
 	)

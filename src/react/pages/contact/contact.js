1c438f3a32ccd47817fc864a358a40d9e417d127
Huzefa <huzefazaheer1+git@gmail.com>
2024-05-26 02:28:50 +0500

    Made contact form work

--- src/react/pages/contact/contact.js
+++ src/react/pages/contact/contact.js
@@ -14,13 +14,38 @@ export default function Contact() {
 	// 	})
 	// }
 
+	const [result, setResult] = React.useState('')
+
+	const onSubmit = async (event) => {
+		event.preventDefault()
+		setResult('Sending....')
+		const formData = new FormData(event.target)
+
+		formData.append('access_key', '2f05312f-ac65-4a57-abc2-91182e9a9855')
+
+		const response = await fetch('https://api.web3forms.com/submit', {
+			method: 'POST',
+			body: formData,
+		})
+
+		const data = await response.json()
+
+		if (data.success) {
+			setResult('Complaint Sent Successfully')
+			event.target.reset()
+		} else {
+			console.log('Error', data)
+			setResult(data.message)
+		}
+	}
+
 	return (
 		<>
 			<div className="contactsec">
 				<div>
 					<h1>Contact Us</h1>
 				</div>
-				<form method="POST" action="https://api.web3forms.com/submit">
+				{/* <form method="POST" action="https://api.web3forms.com/submit">
 					<input
 						type="hidden"
 						name="access_key"
@@ -47,8 +72,36 @@ export default function Contact() {
 						</input>
 					</div>
 				</form>
+				<script
+					src="https://web3forms.com/client/script.js"
+					async
+					defer
+				></script> */}
+				<div>
+					<form onSubmit={onSubmit}>
+						<div className="inputcontainer">
+							<div className="forminput">
+								<label htmlFor="name">Name</label>
+								<input type="text" name="name" required />
+							</div>
+							<div className="forminput">
+								<label htmlFor="email">Email Address</label>
+								<input type="email" name="email" required />
+							</div>
+						</div>
+						<div className="forminput textareainput">
+							<label htmlFor="issue">How can we help?</label>
+							<textarea name="message" required></textarea>
+						</div>
+						<div className="buttongroup">
+							<button type="submit" id="btn_submit" className="button-dark">
+								Submit Form
+							</button>
+						</div>
+					</form>
+					<h3 className="resulttxt">{result}</h3>
+				</div>
 			</div>
-			<script src="https://web3forms.com/client/script.js" async defer></script>
 		</>
 	)
 }
